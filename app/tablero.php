6499869455062998dd1ae9bbdbc1bab254e5ee0b
ManuRC2 <manuelcolusso@live.com.ar>
2022-11-10 01:23:27 -0300

    Mejores tests para tablero

--- app/tablero.php
+++ app/tablero.php
@@ -33,6 +33,7 @@ class Tablero implements InterfazTablero{
         $this->limpiar();
     }
     
+    # poner_ficha -> recibe una columna y una ficha y coloca la ficha en la posicion mas alta disponible de la columna dada
     public function poner_ficha(int $columna, Ficha $ficha)
     {
         $fila = $this->altura($columna);
@@ -41,40 +42,61 @@ class Tablero implements InterfazTablero{
             $this->ult_x = $columna;
             $this->ult_y = $fila;
             $this->ult_ficha = $ficha;
+            return 0;
         }
-        else
-        {
-            print("No se puede colocar una ficha en esa columna.");
-        }
+        print("No se puede colocar una ficha en esa columna.");
+        return 1;
     }
     
+    #  get_ficha -> recibe una columna y (opcional) una fila y devuelve la ficha en esa celda
+    #  si no recibe fila devuelve la ficha mas arriba de la columna
+    #  si se recibe una posición invalida devuelve NULL
     public function get_ficha(int $columna, int $fila = -1)
     {
-        if($fila >= $this->y && $fila <= 0){
-            $fila = $this->altura($columna);
+        if($fila == -1){
+            $fila = $this->altura($columna)-1;
+            return $this->tablero[$columna][$fila];
+        }
+        if(!($this->fila_valida($fila)) || !($this->columna_valida($columna))){
+            return NULL;
         }
         return $this->tablero[$columna][$fila];
+
     }
 
+    # limpiar -> recorre todas las casillas del tablero y reemplaza su valor por NULL
     public function limpiar()
     {
-        for($x=0; $x<$this->x; $x++){
-            for($y=0; $y<$this->y; $y++){
+        for($x=0; $x<=$this->x; $x++){
+            for($y=0; $y<=$this->y; $y++){
                 $this->tablero[$x][$y] = NULL;
             }
         }
     }
 
+    # altura -> recibe una columna y devuelve la primera fila vacia de la misma o -1 si está llena
     public function altura(int $col)
     {
         $disponible = -1;
         $fila_actual = 0;
-        while($disponible < 0 && $fila_actual <= $this->y){
-            if($this->tablero[$col][$fila_actual] == NULL){
-                $disponible = $fila_actual;
+        if($this->columna_valida($col)){
+            while($disponible < 0 && $fila_actual <= $this->y){
+                if(empty($this->tablero[$col][$fila_actual])){
+                    $disponible = $fila_actual;
+                }
+                $fila_actual++;
             }
-            $fila_actual++;
         }
         return $disponible;
+    }
+
+    #  fila_valida -> valida la existencia de una fila dada
+    private function fila_valida($fila){
+        return ($fila <= $this-> y && $fila >= 0);
+    }
+
+    #  columna_valida -> valida la existencia de una columna dada
+    private function columna_valida($columna){
+        return ($columna <= $this-> x && $columna >= 0);
     }
 }
--- tests/Feature/TableroTest.php
+++ tests/Feature/TableroTest.php
@@ -23,7 +23,34 @@ class TestTablero extends TestCase
         $ficha =  new Ficha($color);
         $tablero = new Tablero($x, $y);
         $tablero->poner_ficha($columna, $ficha);
-        $this->assertEquals($ficha, $tablero->get_ficha($columna, $tablero->altura($columna)-1));
+        $this->assertEquals($ficha, $tablero->get_ficha($columna));
     }
 
+    public function test_poner_ficha_invalida()
+    {
+        $x = rand(4, 10);
+        $y = rand(4, 10);
+        $color = rand(1,2);
+        $columna = rand(-10, -1);
+        $ficha =  new Ficha($color);
+        $tablero = new Tablero($x, $y);
+        $this->assertEquals(1, $tablero->poner_ficha($columna, $ficha));
+    }
+
+    public function test_limpiar_tablero(){
+        $x = rand(4, 10);
+        $y = rand(4, 10);
+        $color = rand(1,2);
+        $columna = rand(0, $x);
+        $fila = rand(0, $y);
+        $ficha =  new Ficha($color);
+        $tablero = new Tablero($x, $y);
+        for($i = 0; $i<=$x; $i++){
+            for($j = 0; $j<=$y; $j++){
+                $tablero->poner_ficha($i, $ficha);
+            }
+        }
+        $tablero->limpiar();
+        $this->assertEquals(NULL, $tablero->get_ficha($columna, $fila));
+    }
 }
